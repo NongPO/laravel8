a38639b5002a21d11fcb5068b3be9353545849cb
NongPO <86454957+NongPO@users.noreply.github.com>
2021-07-09 12:56:35 +0700

    First Push to Heroku

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<html>
+
+<body>
+    <h2>Gallerry of Puwadon 62122420021 ภูวดล แพงอ่ออน<h2>
+            <p1>ant</p1>
+            <img src="{{$ant}}" width="250" height="200">
+            <p1>bird</p1>
+            <img src="{{$bird}}" width="250" height="200">
+            <p1>cat</p1>
+            <img src="{{$cat}}" width="250" height="200">
+            <p1>god</p1>
+            <img src="{{$god}}" width="250" height="200">
+            <p1>spider</p1>
+            <img src="{{$spider}}" width="250" height="200">
+            
+</body>
+
+
+
+</body>
+
+</html>
---
+++ resources/views/test/ant.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>   Work 2 Form Aj.Chavalit ///// นายภูวดล แพงอ่ออน 62122420021 </h1>
+ant <img src="{{$ant}}" width="250" height="200">
+</body>
+</html>
